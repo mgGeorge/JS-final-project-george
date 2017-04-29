26004581518d247ce5654a2e7f2c5b424e6ddcc7
mgGeorge <lamborghini01034@gmail.com>
2017-04-29 15:26:15 +0800

    新增背景

---
+++ main.js
@@ -0,0 +1,8 @@
+var bglmg = douument.createElement("img");
+bglmg.src = "images/map.png";
+var canvas = douument.getElementById("game-canvas");
+var cts = canvas.getContext("2d");
+function draw(){
+	ctx.drawImage(bgimg,0,0);
+}
+draw();
